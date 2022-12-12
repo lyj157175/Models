491c896f1fbc5202cb83c784a9f831ec3be6e56d
lyj157175 <lyj157175@163.com>
2022-12-12 14:00:18 +0800

    commit

---
+++ 015_vgg.py
@@ -0,0 +1,64 @@
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import math
+import torch
+
+
+
+class VGG(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(VGG, self).__init__()
+        layers = []
+        in_channels = 3
+        config = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
+        for v in config:
+            if v == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                layers += [conv2d, nn.ReLU(inplace=True)]
+                in_channels = v
+
+        self.features = nn.Sequential(*layers)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+
+if __name__ == '__main__':
+    input_ = torch.randn(1, 3, 224, 224)
+    model = VGG()
+    out = model(input_)
+    print(out.shape)
+
